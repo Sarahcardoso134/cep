fec8c553e50b6798e189ea2aee2b5f3227407f52
Sarahcardoso134 <99329372+Sarahcardoso134@users.noreply.github.com>
2022-09-19 09:23:47 -0300

    Add files via upload

---
+++ cep/src/App.vue
@@ -0,0 +1,30 @@
+<script >
+export default {
+  data()  {
+    return{
+      cep: " ",
+      resultado: {}
+    };
+  },
+};
+methods: {
+  async buscar()  {
+    const url=''
+    this.resultado = await axios.get(url, {
+      headers: {}
+      Authorization: "Token token"
+    )
+  }
+}
+</script>
+
+<template>
+  <main>
+    <h1>Busca CEP</h1>
+    <form @submit.prevent="search">
+    <input type="text" v-model="cep" />
+    <button type="submit">Buscar</button>
+    </form>
+      
+  </main>
+  </template>
